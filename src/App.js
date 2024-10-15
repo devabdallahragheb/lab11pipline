f152d3a5e25d6ad3fec84dd1b1defec13dbb8341
aragheb <abdallah.ragheb@squadio.com>
2024-10-15 22:28:28 +0300

    add new task

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World abdullah test </h1>
+      <h1>Hello World abdullah test  word</h1>
     </div>
   );
 }
